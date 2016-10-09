54420306fb71f5e705ef54ff26aeaefebb13febe
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2016-10-09 21:08:49 +0100

    Add testing structure

---
+++ source/thundax.testproject.dpr
@@ -0,0 +1,14 @@
+program thundax.testproject;
+
+uses
+  Vcl.Forms,
+  uFormTest in 'uFormTest.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
